f15d77854262e6603638988ab30f1423bf66cff9
Daisuke Kobayashi <dreamers1632@gmail.com>
2015-12-09 22:43:34 +0900

    AdditionalReactiveProperty/ReactiveCommand

--- MydnsUpdater/ViewModel/MydnsUpdateWindowViewModel.cs
+++ MydnsUpdater/ViewModel/MydnsUpdateWindowViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
+using Reactive.Bindings.Notifiers;
 using System.ComponentModel.DataAnnotations;
 using System.Reactive.Linq;
 
@@ -15,51 +16,106 @@ namespace MydnsUpdater.ViewModel
         /// <summary>
         /// MydnsのマスターID
         /// </summary>
-        [Required(ErrorMessage = "Error!!")]
+        [Required(ErrorMessage = "必須入力です")]
         public ReactiveProperty<string> MasterId { get; private set; }
 
         /// <summary>
         /// Mydnsのパスワード
         /// </summary>
-        [Required(ErrorMessage = "Error!!")]
         public ReactiveProperty<string> Password { get; private set; }
 
         /// <summary>
-        /// DNS更新ボタン
+        /// Mydnsへの更新間隔
         /// </summary>
-        public ReactiveCommand DnsUpdateCommand { get; }
+        [Required(ErrorMessage = "必須入力です")]
+        [RegularExpression(@"[0-9]+", ErrorMessage = "半角数字のみ入力できます。")]
+        public ReactiveProperty<string> UpdateSpan { get; private set; }
+
+        /// <summary>
+        /// DNS更新のコマンド
+        /// </summary>
+        public ReactiveCommand DnsUpdateCommand { get; private set; }
+
+        /// <summary>
+        /// DNSを一定間隔で更新するコマンド
+        /// </summary>
+        public ReactiveCommand DnsIntervalUpdateCommand { get; private set; }
+
+        /// <summary>
+        /// DNSの一定間隔更新をキャンセルするコマンド
+        /// </summary>
+        public ReactiveCommand DnsCancelIntervalCommand { get; private set; }
+
+        public IObservable<bool> IsProcessing { get; private set; }
 
         /// <summary>
         /// コンストラクタ
         /// </summary>
         public MydnsUpdateWindowViewModel()
         {
-
-            // RequiredAttributeの設定
+            // 書くテキストボックスのバリデーションを有効化
+            // MasterIdとUpdateSpanはRequiredAttribute検証の有効化。
+            // Passwordは独自のバリデーションを設定。
             this.MasterId = new ReactiveProperty<string>()
                 .SetValidateAttribute(() => this.MasterId);
             this.Password = new ReactiveProperty<string>()
-                .SetValidateAttribute(() => this.Password);
+                .SetValidateNotifyError(x => string.IsNullOrEmpty(x) ? "必須入力です" : null);
+            this.UpdateSpan = new ReactiveProperty<string>()
+                .SetValidateAttribute(() => this.UpdateSpan);
 
             // ReactiveCommandの生成
-            // マスターIDとパスワードの両方が入力されていない限りCommandを受け付けない
+            // 更新コマンドはMasterIdとPasswordが正しく入力がされている場合に限りCommandを受け付ける
             this.DnsUpdateCommand = new[]{
                 this.MasterId.ObserveHasErrors,
-                this.Password.ObserveHasErrors
+                this.Password.ObserveHasErrors,
             }
             .CombineLatestValuesAreAllFalse()
             .ToReactiveCommand();
 
+            // インターバルコマンドは全てのテキストボックスが正しく入力がされている
+            // かつインターバル実行中でなければCommandを受け付ける
+            this.DnsIntervalUpdateCommand = new[]
+            {
+                this.MasterId.ObserveHasErrors,
+                this.Password.ObserveHasErrors,
+                this.UpdateSpan.ObserveHasErrors
+            }
+            .CombineLatestValuesAreAllFalse()
+            .ToReactiveCommand();
+
+            // キャンセルコマンドは一定間隔更新コマンドが動いている時のみCommandを受け付ける
+            this.DnsCancelIntervalCommand = new[]
+            {
+                this.IsProcessing
+            }
+            .CombineLatestValuesAreAllTrue()
+            .ToReactiveCommand();
+
+
+            // 購読開始
             this.DnsUpdateCommand.Subscribe(_ =>
             {
                 UpdateMydnsServer();
             });
-        }
 
+            // TODO:テスト
+            InitializeTestMethod();
+
+        }
 
         private void UpdateMydnsServer()
         {
-            Console.WriteLine("この変に簡易Model作る");
+            Console.WriteLine("この辺りにそれっぽいメインロジック作る");
+        }
+
+        // TODO:テスト
+        private void InitializeTestMethod()
+        {
+            var source = Observable.Timer(TimeSpan.FromSeconds(5),TimeSpan.FromSeconds(1));
+            source.Subscribe(_ =>
+            {
+                UpdateMydnsServer();
+            });
         }
 
     }
