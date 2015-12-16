dd3885df9397d1e97029467e5ee3aac77b1b9d7c
Daisuke Kobayashi <dreamers1632@gmail.com>
2015-12-16 22:29:03 +0900

    CreateNetWorkInfomationClass

---
+++ MydnsUpdater/Model/NetWorkInfomation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MydnsUpdater.Model
+{
+    public class NetworkInfomation
+    {
+        public string Ip { get; set; }
+        public string About { get; set; }
+        public string Pro { get; set; }
+    }
+}
