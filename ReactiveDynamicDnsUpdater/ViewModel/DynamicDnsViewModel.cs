3acde0087a5ee5249865fcdd9dc25cbee86a9b36
Daisuke Kobayashi <dreamers1632@gmail.com>
2015-12-31 17:10:15 +0900

    1FileAdd

---
+++ ReactiveDynamicDnsUpdater/ViewModel/DynamicDnsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveDynamicDnsUpdater.Model;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace ReactiveDynamicDnsUpdater.ViewModel
+{
+    internal class DynamicDnsViewModel
+    {
+        public ReactiveProperty<string> Status { get; set; }
+        public ReactiveProperty<string> Time { get; set; }
+
+        private DynamicDnsInfomation Model { get; }
+
+        public DynamicDnsViewModel(DynamicDnsInfomation model)
+        {
+            Model = model;
+            Status = Model
+                //Model=>ViewModelのみバインド(双方向は.ToReactivePropertyAsSynchronized(x => x.Status))
+                .ObserveProperty(x => x.Status)
+                .ToReactiveProperty();
+            Time = Model
+                //Model=>ViewModelのみバインド(双方向は.ToReactivePropertyAsSynchronized(x => x.Time))
+                .ObserveProperty(x => x.Time)
+                .ToReactiveProperty();
+        }
+
+        public DynamicDnsViewModel()
+        {
+        }
+    }
+}
